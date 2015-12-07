47cf9b7bcf1b93ecb2c65059ea7d037b582a6a47
gdomorski <greg.domorski@gmail.com>
2015-12-07 15:21:03 -0800

    Completed Upto fetch

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,43 @@
 // YOUR CODE HERE:
+
+var message = {
+  username: 'Mel Brooks',
+  text: 'It\'s good to be the king',
+  roomname: 'lobby'
+}
+
+
+var app = {
+  init:function(){
+
+  },
+  send:function(){
+    
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type:'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success:function(data){
+
+      },
+      error:function(data){
+
+      }
+    })
+  },
+  fetch: function(){
+    $.ajax({
+      //url:'https://api.parse.com/1/classes/chatterbox/',
+    
+      type:'GET',
+      contentType: 'application/json',
+      success:function(data){
+      },
+      error:function(data){
+
+      }
+    });
+  }
+
+};
