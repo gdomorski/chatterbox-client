706d8f4f57c2e7988ed31d510613ee95e6259430
gdomorski <greg.domorski@gmail.com>
2015-12-07 16:33:23 -0800

    Completed The Specs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,18 +3,27 @@
 var message = {
   username: 'Mel Brooks',
   text: 'It\'s good to be the king',
+  //text: $('#send').val(),
   roomname: 'lobby'
 }
 
 
-var app = {
-  init:function(){
+$('.submit').click(function(){
+  app.handleSubmit();
 
+})
+
+
+var app = {
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  init:function(){
+    $('#send .submit').on('submit', app.handleSubmit)
+    $('.username').on('click',app.addFriend)
   },
   send:function(){
     
     $.ajax({
-      url: 'https://api.parse.com/1/classes/chatterbox',
+      url: app.server,
       type:'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
@@ -28,16 +37,31 @@ var app = {
   },
   fetch: function(){
     $.ajax({
-      //url:'https://api.parse.com/1/classes/chatterbox/',
-    
+      url: app.server,
       type:'GET',
       contentType: 'application/json',
       success:function(data){
+        console.log(data);
       },
       error:function(data){
 
       }
     });
+  },
+  clearMessages:function(){
+    $('#chats').html('')
+  },
+  addMessage: function(message){
+    $('#chats').append('<li class="username">' + message.username + " : " + message.text + '</li>');
+  },
+  addRoom: function(room){
+    $("#roomSelect").append('<option value=' + room +'>' + room + '</option>')
+  },
+  addFriend:function(){
+
+  },
+  handleSubmit:function(){
+    console.log("hello");
   }
 
 };
