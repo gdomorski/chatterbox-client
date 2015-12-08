b126af5ad76a7992b9a65ef8a5788ac4c7023cee
gdomorski <greg.domorski@gmail.com>
2015-12-07 21:03:00 -0800

    Working on Bare minimum requirements

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,67 +1,104 @@
-// YOUR CODE HERE:
+$(function(){
 
-var message = {
-  username: 'Mel Brooks',
-  text: 'It\'s good to be the king',
-  //text: $('#send').val(),
-  roomname: 'lobby'
-}
+  var arr = ['<','>',"&", ';',")","("]
+  var rooms = [];
 
+  $('.submit').on('click', function(event){
+    event.preventDefault();
 
-$('.submit').click(function(){
-  app.handleSubmit();
-
-})
-
-
-var app = {
-  server: 'https://api.parse.com/1/classes/chatterbox',
-  init:function(){
-    $('#send .submit').on('submit', app.handleSubmit)
-    $('.username').on('click',app.addFriend)
-  },
-  send:function(){
-    
-    $.ajax({
-      url: app.server,
-      type:'POST',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success:function(data){
-
-      },
-      error:function(data){
-
-      }
-    })
-  },
-  fetch: function(){
-    $.ajax({
-      url: app.server,
-      type:'GET',
-      contentType: 'application/json',
-      success:function(data){
-        console.log(data);
-      },
-      error:function(data){
-
-      }
+    app.send({
+      username:'pokemon',
+      text:$('#message').val(),
+      roomname:'lobby'
     });
-  },
-  clearMessages:function(){
+  });
+
+
+  $('.update').on('click', function(event){
+    event.preventDefault();
     $('#chats').html('')
-  },
-  addMessage: function(message){
-    $('#chats').append('<li class="username">' + message.username + " : " + message.text + '</li>');
-  },
-  addRoom: function(room){
-    $("#roomSelect").append('<option value=' + room +'>' + room + '</option>')
-  },
-  addFriend:function(){
+    app.fetch();
+  });
 
-  },
-  handleSubmit:function(){
-    console.log("hello");
-  }
 
-};
+  window.app = {
+    server: 'https://api.parse.com/1/classes/chatterbox',
+    init:function(){
+      $('#send .submit').on('submit', app.handleSubmit)
+      $('.username').on('click',app.addFriend)
+    },
+    send:function(msg){
+      
+      $.ajax({
+        url: app.server,
+        type:'POST',
+        data: JSON.stringify(msg),
+        contentType: 'application/json',
+        success:function(data){
+          $('#chats').html('');
+          $('.update').trigger('click');
+        },
+        error:function(data){
+
+        }
+      })
+    },
+    fetch: function(){
+      $.ajax({
+        url: app.server,
+        type:'GET',
+        contentType: 'application/json',
+        success:function(data){
+          data.results.reverse();
+          app.addMessage(data.results);
+        },
+        error:function(data){
+        }
+      });
+    },
+    clearMessages:function(){
+      $('#chats').html('')
+    },
+    addMessage: function(message){
+      
+      _.each(message,function(msg){
+        app.addRoom(msg.roomname)
+
+        var ourMessage = '<li class="username">' + app.checkMessage(msg.username) + " : " + app.checkMessage(msg.text) + '</li>'
+        $('#chats').prepend(ourMessage);
+      });
+
+    },
+    addRoom: function(room){
+      if(rooms.indexOf(room) === -1){
+       $("#roomSelect").append('<option value=' + room +'>' + room + '</option>')
+       rooms.push(room)
+      }
+
+    },
+    addFriend:function(){
+
+    },
+    handleSubmit:function(){
+      
+    },
+    checkMessage:function(message){
+        if(typeof message !== 'string'){
+          return;
+        }
+
+      for(var i = 0; i < message.length; i++){
+        if(arr.indexOf(message[i]) !== -1){
+          return "Invalid Message";
+        }
+      }
+      return message;
+    }
+
+  };
+
+  app.fetch();
+
+
+});
+
